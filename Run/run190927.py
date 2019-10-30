80f7278db7262e887e0f95f6173231ac40361956
sdu20 <sdu2014@126.com>
2019-10-30 20:51:16 +0800

    update

--- Run/run190927.py
+++ Run/run190927.py
@@ -542,7 +542,7 @@ def run_test(data_name0=None):
     main_path = "E:\\Project\\result2019\\result0927\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "bostonHouse"
+    data_name = "bostonHouse-6912"
     if data_name0 is None:
         pass
     else:
@@ -552,7 +552,7 @@ def run_test(data_name0=None):
     yita = 0.1
     nbrs_k = 20
     method_k = 20
-    eigen_numbers = 4
+    eigen_numbers = 3
     draw_kind = "b-spline"
     normalize = True
     straighten = False  # 是否进行校直操作
@@ -573,8 +573,8 @@ def run_test(data_name0=None):
     (n, m) = data_shape(main_path, data_name)
     if method == "P_matrix":
         P_matrix = np.zeros((m, 2))
-        x_index = 5  # 第一个维度
-        y_index = 13  # 第二个维度
+        x_index = 0  # 第一个维度
+        y_index = 2  # 第二个维度
         P_matrix[x_index, 0] = 1
         P_matrix[y_index, 1] = 1
 
--- Test/cleanData.py
+++ Test/cleanData.py
@@ -1,5 +1,9 @@
 # 清洗数据
 import numpy as np
+from Main import DimReduce
+from Main import Preprocess
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 
 def wine_quality_red():
@@ -15,5 +19,46 @@ def wine_quality_red():
     print('数据清洗完毕')
 
 
+def bostonHouse6912():
+    path = "E:\\Project\\result2019\\result1026without_straighten\\datasets\\bostonHouse\\"
+    data = np.loadtxt(path+"data.csv", dtype=np.float, delimiter=",")
+    (n, m) = data.shape
+
+    data2 = np.zeros((n, 3))
+    data2[:, 0] = data[:, 5]
+    data2[:, 1] = data[:, 8]
+    data2[:, 2] = data[:, 13]
+
+    np.savetxt(path+"data2.csv", data2, fmt="%f", delimiter=",")
+    print("数据处理完毕")
+
+
+def coil_20():
+    path = "E:\\Project\\result2019\\result1026without_straighten\\datasets\\coil20obj\\"
+    data = np.loadtxt(path+"data.csv", dtype=np.float, delimiter=",")
+    label = np.loadtxt(path+"label.csv", dtype=np.int, delimiter=",")
+    (n, m) = data.shape
+    X = Preprocess.normalize(data, -1, 1)
+
+    # dr_method = "PCA"
+    # Y = DimReduce.dim_reduce(X[0:5*72, :], method=dr_method, method_k=15)
+    # plt.scatter(Y[:, 0], Y[:, 1], marker='o', c=label[0:5*72])
+
+    # Y = DimReduce.dim_reduce(X, method=dr_method, method_k=15)
+    # plt.scatter(Y[:, 0], Y[:, 1], marker='o', c=label)
+    # plt.title(dr_method)
+    # ax = plt.gca()
+    # ax.set_aspect(1)
+    # plt.show()
+
+    # 用PCA将数据压缩到 16 维
+    pca = PCA(n_components=16)
+    Y = pca.fit_transform(X)
+    np.savetxt(path+"Y16.csv", Y, fmt='%f', delimiter=",")
+    print("数据处理完毕")
+
+
 if __name__ == '__main__':
-    wine_quality_red()
+    # wine_quality_red()
+    # bostonHouse6912()
+    coil_20()
