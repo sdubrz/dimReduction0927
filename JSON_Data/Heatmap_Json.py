871da251b69d93bea5c114238479114d7bd1670e
sdu20 <sdu2014@126.com>
2019-11-19 09:31:15 +0800

    heatmap finished

--- JSON_Data/Heatmap_Json.py
+++ JSON_Data/Heatmap_Json.py
@@ -21,25 +21,36 @@ def create_json(path="", dim_name=[]):
 
     temp_item = old_data[0]
     dim = temp_item['dNum']
-    if len(dim_name)==0:
+    if len(dim_name) == 0:
         for i in range(0, dim):
             dim_name.append("x"+str(i))
 
     file_writer = open(path+"heatmap.json", 'w', encoding='UTF-8')
-    file_writer.write("{\"scatters\": {[")
+    file_writer.write("{\"scatters\": [")
+    n = len(old_data)
+    index = 0
     for item in old_data:
-        file_writer.write(str(item) + "\n")
-    file_writer.write("]}, \n")
+        file_writer.write((str(item)).replace("\'", "\""))
+        if index != n-1:
+            file_writer.write(",\n")
+        index += 1
+    file_writer.write("], \n")
     file_writer.write("\"heatmap\": {")
-    file_writer.write("\"left_low_x\": " + str(heat_param['left_low_x']) + ",\n")
-    file_writer.write("\"left_low_y\": " + str(heat_param['left_low_y']) + ",\n")
-    file_writer.write("\"rows\": " + str(heat_param['rows']) + ",\n")
-    file_writer.write("\"columns\": " + str(heat_param['columns'] + ",\n"))
-    file_writer.write("\"r\": " + str(heat_param['r']) + ",\n")
-    dim_name_line = "\"dim_name\": " + str(dim_name) + ",\n"
-    # 还差各个heatmap没写
+    file_writer.write("\"left_low_x\": " + str(heat_param['left_low_x']) + ",\n")  # 左下方网格的左下方顶点横坐标
+    file_writer.write("\"left_low_y\": " + str(heat_param['left_low_y']) + ",\n")  # 左下方网格的左下方顶点纵坐标
+    file_writer.write("\"rows\": " + str(heat_param['rows']) + ",\n")  # 网格的行数
+    file_writer.write("\"columns\": " + str(heat_param['columns']) + ",\n")  # 网格的列数
+    file_writer.write("\"r\": " + str(heat_param['r']) + ",\n")  # 网格的边长
+    file_writer.write(("\"dim_name\": " + str(dim_name) + ",\n").replace("\'", "\""))  # 每个属性的名称
+    file_writer.write("\"arrays\": [")
+    for i in range(0, dim):  # 每个属性的 heatmap对应一个二维矩阵，矩阵中相应的位置对应着一个网格中该属性的值
+        map_matrix = np.loadtxt(heat_path+str(i)+".csv", dtype=np.float, delimiter=",")
+        file_writer.write(str(map_matrix.tolist()))
+        if i != dim-1:
+            file_writer.write(",\n")
+    file_writer.write("]")
+    file_writer.write("}\n")  # heatmap数据至此终
 
-    file_writer.write("}\n")
     file_writer.write("}")
 
     file_writer.close()
