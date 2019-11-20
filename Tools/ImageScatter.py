36121a725f8706d71e1a63e4b5fe1365a16c86c3
sdu20 <sdu2014@126.com>
2019-11-20 10:26:59 +0800

    show images in scatter plot

--- Tools/ImageScatter.py
+++ Tools/ImageScatter.py
@@ -5,22 +5,25 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 from PIL import Image
+import os
 
 
-def small_image(eta=0.5):
+def small_image(eta=0.5, in_path="", out_path=""):
     """
     将图片缩小
     :param eta: 缩小的倍数
+    :param in_path: 读取图片路径
+    :param out_path: 保存缩小后的图片的路径
     :return:
     """
-    path1 = "E:\\Project\\DataLab\\duck\\images\\"
-    path2 = "E:\\Project\\DataLab\\duck\\smallimages\\"
-
-    for i in range(0, 72):
-        im = Image.open(path1+"obj1__"+str(i)+".png")
-        (x, y) = im.size
-        s_img = im.resize((int(x*eta), int(y*eta)), Image.ANTIALIAS)
-        s_img.save(path2+"obj1__"+str(i)+".png")
+    # path1 = "E:\\Project\\DataLab\\duck\\images\\"
+    # path2 = "E:\\Project\\DataLab\\duck\\smallimages\\"
+    for root, middle, files in os.walk(in_path):
+        for file_name in files:
+            im = Image.open(in_path+file_name)
+            (x, y) = im.size
+            s_img = im.resize((int(x*eta), int(y*eta)), Image.ANTIALIAS)
+            s_img.save(out_path+file_name)
 
 
 def get_image(path):
@@ -29,7 +32,9 @@ def get_image(path):
 
 def image_scatter():
     path = "E:\\Project\\DataLab\\duck\\"
-    small_image(eta=0.3)
+    path1 = "E:\\Project\\DataLab\\duck\\images\\"
+    path2 = "E:\\Project\\DataLab\\duck\\smallimages\\"
+    small_image(eta=0.3, in_path=path1, out_path=path2)
     image_path = path + "smallimages\\obj1__"
     X = np.loadtxt(path+"data.csv", dtype=np.float, delimiter=",")
     X = preprocessing.minmax_scale(X)
@@ -49,6 +54,28 @@ def image_scatter():
     plt.show()
 
 
-if __name__ == '__main__':
-    image_scatter()
+def coil_image_scatter():
+    """在coil数据的降维结果中展示图片"""
+    path = "E:\\Project\\DataLab\\imageScatter\\"
+    small_image(eta=0.15, in_path=path+"images\\", out_path=path+"smallImages\\")
+    Y = np.loadtxt(path+"mds2.csv", dtype=np.float, delimiter=",")
+    (n, m) = Y.shape
+    fig, ax = plt.subplots()
+    ax.scatter(Y[:, 0], Y[:, 1])
+    for i in range(0, n):
+        obj = i // 72 + 1
+        img_index = i % 72
+        ab = AnnotationBbox(get_image(path+"smallImages\\obj"+str(obj)+"__"+str(img_index)+".png"), (Y[i, 0], Y[i, 1]), frameon=False)
+        ax.add_artist(ab)
+    ax = plt.gca()
+    ax.set_aspect(1)
+    plt.show()
 
+
+if __name__ == '__main__':
+    # image_scatter()
+    # path1 = "E:\\Project\\DataLab\\duck\\images\\"
+    # for i, j, k in os.walk(path1):
+    #     for file in k:
+    #         print(file)
+    coil_image_scatter()
