277f5822d64eaa6149ded9a4cb2a6076230ee1d8
sdu20 <sdu2014@126.com>
2019-11-21 16:30:11 +0800

    cluster data

--- ClusterTest/clusterTest.py
+++ ClusterTest/clusterTest.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
+from sklearn.mixture import GaussianMixture
 
 
 COLORS = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'orange', 'deepskyblue']
@@ -36,10 +37,39 @@ def dbscan_data(path):
     X = np.loadtxt(path + "x.csv", dtype=np.float, delimiter=",")
     Y = np.loadtxt(path + "y.csv", dtype=np.float, delimiter=",")
     (n, m) = X.shape
-    
+    clustering = DBSCAN(eps=0.4, min_samples=10).fit(X)
+    label = clustering.labels_
+    print(max(label))
+    plt.scatter(Y[:, 0], Y[:, 1], c=label)
+    ax = plt.gca()
+    ax.set_aspect(1)
+    plt.show()
+
+
+def gauss_mix_data(path):
+    """
+    用高斯混合距离
+    :return:
+    """
+    X = np.loadtxt(path + "x.csv", dtype=np.float, delimiter=",")
+    Y = np.loadtxt(path + "y.csv", dtype=np.float, delimiter=",")
+    (n, m) = X.shape
+    gauss = GaussianMixture(n_components=8)
+    label = gauss.fit_predict(X)
+    print(max(label))
+    # plt.scatter(Y[:, 0], Y[:, 1], c=label)
+    for i in range(0, n):
+        c_index = label[i] % len(COLORS)
+        plt.scatter(Y[i, 0], Y[i, 1], c=COLORS[c_index])
+    ax = plt.gca()
+    ax.set_aspect(1)
+    plt.title("GaussianMix")
+    plt.show()
 
 
 if __name__ == '__main__':
     path = "E:\\Project\\result2019\\result1026without_straighten\\PCA\\Wine\\yita(0.05)nbrs_k(30)method_k(30)numbers(4)_b-spline_weighted\\"
     olive_path = "E:\\Project\\result2019\\result1026without_straighten\\PCA\\olive\\yita(0.03)nbrs_k(45)method_k(20)numbers(4)_b-spline_weighted\\"
-    k_means_data(olive_path)
+    # k_means_data(olive_path)
+    # dbscan_data(olive_path)
+    gauss_mix_data(olive_path)
