b2d97d968f5d9dff978bc4cdc514c42f923d6cd7
sdu20 <sdu2014@126.com>
2019-11-21 16:32:41 +0800

    cluster data

--- ClusterTest/clusterTest.py
+++ ClusterTest/clusterTest.py
@@ -9,23 +9,28 @@ from sklearn.mixture import GaussianMixture
 COLORS = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'orange', 'deepskyblue']
 
 
-def k_means_data(path):
+def k_means_data(path, n_cluster=8, draw=False):
     """
     对数据进行k_means聚类
     :param path: 数据存放的目录
+    :param n_cluster: 簇数
+    :param draw: 是否画出聚类效果散点图
     :return:
     """
     X = np.loadtxt(path+"x.csv", dtype=np.float, delimiter=",")
     Y = np.loadtxt(path+"y.csv", dtype=np.float, delimiter=",")
     (n, m) = X.shape
-    kmeans = KMeans(n_clusters=9).fit(X)
+    kmeans = KMeans(n_clusters=n_cluster).fit(X)
     label = kmeans.labels_
-    for i in range(0, n):
-        c_index = label[i] % len(COLORS)
-        plt.scatter(Y[i, 0], Y[i, 1], c=COLORS[c_index])
-    ax = plt.gca()
-    ax.set_aspect(1)
-    plt.show()
+    if draw:
+        for i in range(0, n):
+            c_index = label[i] % len(COLORS)
+            plt.scatter(Y[i, 0], Y[i, 1], c=COLORS[c_index])
+        ax = plt.gca()
+        ax.set_aspect(1)
+        plt.show()
+    
+    return label
 
 
 def dbscan_data(path):
