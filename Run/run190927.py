6496b600106221ccf030c3727b3160063390f4e0
sdu20 <sdu2014@126.com>
2019-12-06 10:05:09 +0800

    NORB

--- Run/run190927.py
+++ Run/run190927.py
@@ -552,14 +552,14 @@ def run_test(data_name0=None):
     main_path = "E:\\Project\\result2019\\result0927\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "olivettifaces30m"
+    data_name = "norb_test"
     if data_name0 is None:
         pass
     else:
         data_name = data_name0
 
-    method = "PCA"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
-    yita = 0.3
+    method = "MDS"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
+    yita = 100
     nbrs_k = 35
     method_k = 30
     eigen_numbers = 4
