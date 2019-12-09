c98398a8236e6c9ff41e2f9656072645c713cdef
sdu20 <sdu2014@126.com>
2019-12-09 21:13:10 +0800

    Monkey Saddle data

--- Run/run190927.py
+++ Run/run190927.py
@@ -552,13 +552,13 @@ def run_test(data_name0=None):
     main_path = "E:\\Project\\result2019\\result0927\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "MobiusStrip"
+    data_name = "MonekySaddle"
     if data_name0 is None:
         pass
     else:
         data_name = data_name0
 
-    method = "PCA"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
+    method = "MDS"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
     yita = 0.1
     nbrs_k = 20
     method_k = 30
