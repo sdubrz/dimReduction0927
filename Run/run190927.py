15d406edadf0d99afb4ed693b312929a9fcdbab4
sdu20 <sdu2014@126.com>
2019-12-09 20:26:06 +0800

    Möbius Strip

--- Run/run190927.py
+++ Run/run190927.py
@@ -552,7 +552,7 @@ def run_test(data_name0=None):
     main_path = "E:\\Project\\result2019\\result0927\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "twoSphere"
+    data_name = "MobiusStrip"
     if data_name0 is None:
         pass
     else:
@@ -560,7 +560,7 @@ def run_test(data_name0=None):
 
     method = "PCA"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
     yita = 0.1
-    nbrs_k = 10
+    nbrs_k = 20
     method_k = 30
     eigen_numbers = 3
     draw_kind = "b-spline"
