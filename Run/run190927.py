b5a76ae5a3981993cba7e3daada7abf2b13e4cd2
sdu20 <sdu2014@126.com>
2019-12-13 10:43:13 +0800

    update

--- Run/run190927.py
+++ Run/run190927.py
@@ -552,19 +552,19 @@ def run_test(data_name0=None):
     main_path = "E:\\Project\\result2019\\result0927\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "spheresBigSmall"
+    data_name = "MNIST50mclass1_985"
     if data_name0 is None:
         pass
     else:
         data_name = data_name0
 
-    method = "MDS"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
-    yita = 0.1
-    nbrs_k = 20
-    method_k = 30
-    eigen_numbers = 3
+    method = "tsne"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA"
+    yita = 0.001
+    nbrs_k = 90
+    method_k = nbrs_k
+    eigen_numbers = 4
     draw_kind = "b-spline"
-    normalize = False
+    normalize = True
     straighten = False  # 是否进行校直操作
     weighted = True  # 当使用特征向量作为扰动的时候是否添加权重
     P_matrix = None  # 普通的线性降维方法的投影矩阵
