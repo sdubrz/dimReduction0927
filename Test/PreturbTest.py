aa71f13c28e5f516ff4140b526ffb249294cec5a
sdu20 <sdu2014@126.com>
2019-12-13 21:26:34 +0800

    经典的 t-SNE的扰动稳定性测试

--- Test/PreturbTest.py
+++ Test/PreturbTest.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Main import DimReduce
+from MyDR import classTSNE
 
 
 def preturb():
@@ -22,6 +23,19 @@ def preturb():
     plt.scatter(Y3[:, 0], Y3[:, 1], c='b')
 
     plt.show()
+
+
+def class_t_sne_preturb():
+    path = "E:\\Project\\result2019\\result1026without_straighten\\tsne\\Iris\\yita(0.0)nbrs_k(20)method_k(20)numbers(4)_b-spline_weighted\\"
+    # path = "E:\\Project\\result2019\\result1026without_straighten\\tsne\\MNIST50mclass1_985\\yita(0.001)nbrs_k(90)method_k(90)numbers(4)_b-spline_weighted\\"
+    X = np.loadtxt(path + "x.csv", dtype=np.float, delimiter=",")
+    vectors = np.loadtxt(path + "【weighted】eigenvectors0.csv", dtype=np.float, delimiter=",")
+
+    yita = 0.0
+    k = 90
+    X2 = X + yita * vectors
+
+    t_sne = classTSNE.tsne()
 
 
 if __name__ == '__main__':
