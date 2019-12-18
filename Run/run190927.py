f12ce5ce6359686921a1fe196bfc6641559d590b
sdu20 <sdu2014@126.com>
2019-12-18 09:31:35 +0800

    update

--- Run/run190927.py
+++ Run/run190927.py
@@ -552,15 +552,15 @@ def run_test(data_name0=None):
     main_path = "E:\\Project\\result2019\\result0927\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "MNIST50mclass1_985"
+    data_name = "coil20obj_16_3class"
     if data_name0 is None:
         pass
     else:
         data_name = data_name0
 
-    method = "PCA"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA" "LTSA" "cTSNE"
+    method = "cTSNE"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA" "LTSA" "cTSNE"
     yita = 0.1
-    nbrs_k = 60
+    nbrs_k = 30
     method_k = nbrs_k
     eigen_numbers = 4  # 无用
     draw_kind = "b-spline"
