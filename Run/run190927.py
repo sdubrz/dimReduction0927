f6d6ea599a441f681bf87bd103470ef24c767ac2
sdu20 <sdu2014@126.com>
2020-01-04 10:08:54 +0800

    update

--- Run/run190927.py
+++ Run/run190927.py
@@ -582,26 +582,26 @@ def run_test(data_name0=None):
             digits5_8
         """
     start_time = time()
-    main_path_without_normalize = "E:\\project\\result2019\\result1112without_normalize\\"  # 华硕
+    main_path_without_normalize = "E:\\project\\result2020\\result0104without_normalize\\"  # 华硕
     main_path_without_straighten = "E:\\Project\\result2020\\result0103\\"  # 华硕
     # main_path_without_straighten = "E:\\文件\\IRC\\特征向量散点图项目\\result2019\\result1219without_straighten\\"  # XPS
     # main_path = "F:\\result2019\\result0927\\"  # HP
     main_path = "E:\\Project\\result2020\\result0103\\"  # 华硕
     # main_path = 'D:\\文件\\IRC\\特征向量散点图项目\\result2019\\result0927\\'  # XPS
 
-    data_name = "MNIST50mclass1_985"  # coil20obj_16_3class  MNIST50mclass1_985
+    data_name = "fashion50mclass568"  # coil20obj_16_3class  MNIST50mclass1_985  fashion50mclass568
     if data_name0 is None:
         pass
     else:
         data_name = data_name0
 
     method = "MDS"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA" "LTSA" "cTSNE"
-    yita = 0.1
+    yita = 50.0
     nbrs_k = 51
     method_k = nbrs_k
     eigen_numbers = 4  # 无用
     draw_kind = "b-spline"
-    normalize = True
+    normalize = False
     min_proportion = 0.9
     min_good_points = 0.9
     y_precomputed = True  # y是否已经提前计算好
