16c5c113f9b6903fc1d4cddbc31d4aebe3619539
sdubrz <sdu2014@126.com>
2020-02-01 19:34:48 +0800

    attribute colormap scatterplot

--- Run/run190927.py
+++ Run/run190927.py
@@ -590,19 +590,19 @@ def run_test(data_name0=None):
     main_path = 'E:\\文件\\IRC\\特征向量散点图项目\\result2020\\result0119\\'  # XPS
     main_path_without_normalize = 'E:\\文件\\IRC\\特征向量散点图项目\\result2020\\result0119_withoutnormalize\\'  # XPS
 
-    data_name = "pendigits1499"  # coil20obj_16_3class  MNIST50mclass1_985  fashion50mclass568
+    data_name = "olive"  # coil20obj_16_3class  MNIST50mclass1_985  fashion50mclass568
     if data_name0 is None:
         pass
     else:
         data_name = data_name0
 
-    method = "cTSNE"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA" "LTSA" "cTSNE"
-    yita = 0.1
+    method = "MDS"  # "PCA" "MDS" "P_matrix" "Isomap" "LDA" "LTSA" "cTSNE"
+    yita = 10
     nbrs_k = 30
     method_k = 90  # if cTSNE perplexity=method_k/3
     eigen_numbers = 4  # 无用
     draw_kind = "b-spline"
-    normalize = True  # 是否进行normalize
+    normalize = False  # 是否进行normalize
     min_proportion = 0.9
     min_good_points = 0.9
     y_precomputed = False  # y是否已经提前计算好
---
+++ Tools/OriginalColorMap.py
@@ -0,0 +1,29 @@
+# 根据数据原始的维度属性值，在降维结果散点图中用colormap表示数据的某个原始维度的值的分布情况
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def color_scatter():
+    """
+    用颜色表示维度值
+    :return:
+    """
+    # path = "E:\\文件\\IRC\\特征向量散点图项目\\result2020\\result0119_withoutnormalize\\datasets\\olive\\"
+    path = "E:\\文件\\IRC\\特征向量散点图项目\\result2020\\result0119\\datasets\\seeds\\"
+    attr = 3  # 第几个维度
+
+    data = np.loadtxt(path+"data.csv", dtype=np.float, delimiter=",")
+    Y = np.loadtxt(path+"PCA.csv", dtype=np.float, delimiter=",")  # 文件名由降维方法确定
+
+    plt.scatter(Y[:, 0], Y[:, 1], c=data[:, attr])
+    plt.title(str(attr))
+    plt.colorbar()
+    ax = plt.gca()
+    ax.set_aspect(1)
+    plt.show()
+
+
+if __name__ == "__main__":
+    color_scatter()
+
+
